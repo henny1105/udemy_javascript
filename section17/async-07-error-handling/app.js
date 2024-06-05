3924068f1ef3a1fb46f5f32a03ac839575e0cbd1
henny <kimhyein1105@gmail.com>
2024-06-05 15:29:29 +0900

    Async/ await

--- section17/async-07-error-handling/app.js
+++ section17/async-07-error-handling/app.js
@@ -25,28 +25,34 @@ const setTimer = (duration) => {
 	return promise;
 };
 
-function trackUserHandler() {
-	let positionData;
-	getPosition()
-		.then((posData) => {
-			positionData = posData;
-			return setTimer(2000);
-		})
-		.catch((err) => {
-			// 프로미스 체인에서 발생한 오류를 잡아내어 처리함
-			console.log(err);
-			return 'on we go...';
-		})
-		// catch 블록에서 반환된 값은 다음 then 블록으로 전달
-		// 체인을 중단하지 않고 계속 이어나갈 수 있음
-		// 완전히 멈추고 싶다면 catch를 끝으로 옮겨서 강제로 멈출 수 있음
-		.then((data) => {
-			console.log(data, positionData);
-		});
-	setTimer(1000).then(() => {
-		console.log('Timer done!');
-	});
-	console.log('Getting position...');
+async function trackUserHandler() {
+	// let positionData;
+	const posData = await getPosition(); // 프로미스가 해결되거나 실패하기를 기다림
+	const timerData = await setTimer(2000);
+	console.log(timerData, posData);
+	// 그리고 다음 줄이 실행
+	// .then((posData) => {
+	// 	positionData = posData;
+	// 	return setTimer(2000);
+	// })
+	// 	.catch((err) => {
+	// 		// 프로미스 체인에서 발생한 오류를 잡아내어 처리함
+	// 		console.log(err);
+	// 		return 'on we go...';
+	// 	})
+	// 	// catch 블록에서 반환된 값은 다음 then 블록으로 전달
+	// 	// 체인을 중단하지 않고 계속 이어나갈 수 있음
+	// 	// 완전히 멈추고 싶다면 catch를 끝으로 옮겨서 강제로 멈출 수 있음
+	// 	.then((data) => {
+	// 		console.log(data, positionData);
+	// 	});
+	// setTimer(1000).then(() => {
+	// 	console.log('Timer done!');
+	// });
+	// console.log('Getting position...');
 }
 
 button.addEventListener('click', trackUserHandler);
+
+// async는 함수 안에서만 사용할 수 있음
+// async는 실행 방식을 바꾸는 것이 아니라 코드를 내부적으로 반환함
